beb8709b4f937eafd2ab90c25eb99e0adceef21c
angelodel80 <angelodel80@gmail.com>
2013-09-09 17:11:25 +0200

    Sentences with xpath

--- ClaviusLemmata/src/ilc/cnr/it/clavius/ClaviusMain.java
+++ ClaviusLemmata/src/ilc/cnr/it/clavius/ClaviusMain.java
@@ -3,6 +3,7 @@
  */
 package ilc.cnr.it.clavius;
 
+import ilc.cnr.it.clavius.corpus.TextHandler;
 import ilc.cnr.it.clavius.corpus.TreeBankHandler;
 
 import java.io.IOException;
@@ -15,7 +16,7 @@ import java.util.Map;
 public class ClaviusMain {
 
 	private String msg = "";
-	
+
 	/**
 	 * 
 	 */
@@ -23,11 +24,11 @@ public class ClaviusMain {
 		// TODO Auto-generated constructor stub
 		this.msg = "this is the default message!";
 	}
-	
+
 	public ClaviusMain(String msg){
 		this.msg = msg;
 	}
-	
+
 	public void printMsg(){
 		System.out.println(this.msg);
 	}
@@ -35,7 +36,7 @@ public class ClaviusMain {
 	/**
 	 * @param args
 	 */
-	
+
 	private void process(String pathModel, String pathBin) {
 		// TODO Auto-generated method stub
 		HunposTagger hunPos = new HunposTagger(pathModel, pathBin);
@@ -46,38 +47,60 @@ public class ClaviusMain {
 
 	public void manageCorpus(){
 		TreeBankHandler tbh = new TreeBankHandler("ldt-1.5.xml");
-//		try {
-//			tbh.printFile();
-//			
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		//		try {
+		//			tbh.printFile();
+		//			
+		//		} catch (IOException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
 		tbh.transformToTrain();
 	}
-	
+
 	public void printEnv (){
-        Map<String, String> env = System.getenv();
-        for (String envName : env.keySet()) {
-            System.out.format("%s=%s%n",
-                              envName,
-                              env.get(envName));
-        }
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+			System.out.format("%s=%s%n",
+					envName,
+					env.get(envName));
+		}
 	}
-	
+
 	/**
 	 * @param args
 	 */
+
+
+	/**
+	 * @return the msg
+	 */
+	public String getMsg() {
+		return msg;
+	}
+
+	/**
+	 * @param msg the msg to set
+	 */
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ClaviusMain main1 = new ClaviusMain();
 		ClaviusMain main2 = new ClaviusMain("In nova fert animus mutatas dicere formas corpora");
-		
+
 		//main1.printMsg();
 		main2.printMsg();
 		//main2.manageCorpus();
-		//main2.process("en_wsj.model","");
-		main2.process("testFirst.model","");
-	}	
+		main2.process("en_wsj.model","");
+		TextHandler th = new TextHandler();
+		Map<String, String> sentences = th.getSentences("/Users/angelodel80/Risorse/sources/clavius/testXpathExpression.xml");
+		Object[] sents = sentences.values().toArray();
+		for(int i = 0; i< sents.length; i++){
+			main2.setMsg((String)sents[i]);
+			main2.process("testFirst.model","");
+		}
+	}
+
 }
--- ClaviusLemmata/src/ilc/cnr/it/clavius/corpus/TextHandler.java
+++ ClaviusLemmata/src/ilc/cnr/it/clavius/corpus/TextHandler.java
@@ -3,6 +3,23 @@
  */
 package ilc.cnr.it.clavius.corpus;
 
+import ilc.cnr.it.clavius.utils.TextUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.filter.ElementFilter;
+import org.jdom2.filter.Filter;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+
 /**
  * @author angelodel80
  *
@@ -14,6 +31,47 @@ public class TextHandler {
 	 */
 	public TextHandler() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public Map<String, String> getSentences(String filePath){
+		Map<String,String> sentences = null;
+		try {
+			Document doc = TextUtils.fileToDocument(filePath);
+			Namespace ns = doc.getRootElement().getNamespace();
+			XPathExpression<Element> sentencesExpression = 
+					XPathFactory.instance().compile("//s", Filters.element());
+
+			List<Element> listOfSentences = sentencesExpression.evaluate(doc);
+
+			if (null != listOfSentences){
+				sentences = new HashMap<String, String>();
+				for (Element sent : listOfSentences) {
+					String[] sentence = getSentence(sent);
+					sentences.put(sentence[0], sentence[1]);
+				}
+
+			}
+
+
+
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		return sentences;
+	}
+
+	public String[] getSentence(Element sent){
+		String sentence[] = new String[2];
+		sentence[0] = sent.getAttributeValue("n");
+		sentence[1] = sent.getTextNormalize();
+		return sentence;
+
 	}
 
 }
