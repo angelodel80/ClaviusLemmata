505d22658c4cb82ebd917ce3f3b5e8cee569ffbf
angelodel80 <angelodel80@gmail.com>
2013-09-09 15:33:15 +0200

    texthandle new class

---
+++ ClaviusLemmata/src/ilc/cnr/it/clavius/corpus/TextHandler.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ilc.cnr.it.clavius.corpus;
+
+/**
+ * @author angelodel80
+ *
+ */
+public class TextHandler {
+
+	/**
+	 * 
+	 */
+	public TextHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
